44a05f74580cb5705d45ceb2f1bb150f21c3770d
Boris <stealer9119@gmail.com>
2022-04-02 17:14:42 +0300

    lesson1

--- js/script.js
+++ js/script.js
@@ -1,2 +1,17 @@
-let Name = prompt("what is your name","");
-alert(`Hello ${Name}`);
+"use strics";
+let NumberOnFilms = +prompt("Сколько фильмов вы уже посмотрели?","");
+let PersonalMuvieDB = {
+    count: NumberOnFilms,
+    muvies: {},
+    actors: {},
+    genres: {},
+    privat: false
+};
+let a = prompt("Один из последних просмотренных фильмов?",""),
+    b = prompt("На сколько оцените его?"),
+    c = prompt("Один из последних просмотренных фильмов?",""),
+    d = prompt("На сколько оцените его?");
+PersonalMuvieDB.muvies[a] = b;
+PersonalMuvieDB.muvies[c] = d;
+
+console.log(PersonalMuvieDB);
